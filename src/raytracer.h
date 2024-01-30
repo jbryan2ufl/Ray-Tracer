7ab5fc80a84f768ce4e925ad9e0f71aca3589bb2
erstwhilest <jbryan2@ufl.edu>
2024-01-30 00:45:31 -0500

    removed files in .gitignore

--- src/raytracer.h
+++ src/raytracer.h
@@ -30,8 +30,6 @@ struct ray_tracer
 		point_lights.push_back(point_light{glm::vec3{0.0f, 0.5f, -3.0f}});
 		point_lights.push_back(point_light{glm::vec3{1.0f, 0.0f, -3.0f}});
 		point_lights.push_back(point_light{glm::vec3{1.0f, 2.0f, 2.0f}});
-		// point_lights.push_back(point_light{glm::vec3{-1.0f, 0.0f, -3.0f}});
-		// point_lights.push_back(point_light{glm::vec3{0.0f, 3.0f, 0.0f}});
 	}
 
 	void update_image()
